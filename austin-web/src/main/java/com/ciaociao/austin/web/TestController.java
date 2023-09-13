633d2d937cdbe83d2cd7f497d0841846eb3e44c9
CIA0CIAO <navicat18@gmail.com>
2023-09-13 09:05:14 +0800

    add：引入日志框架logback

--- austin-web/src/main/java/com/ciaociao/austin/web/TestController.java
+++ austin-web/src/main/java/com/ciaociao/austin/web/TestController.java
@@ -1,17 +1,20 @@
 package com.ciaociao.austin.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author ciaociao
  */
-
+@Slf4j
 @RestController
 public class TestController {
 
     @RequestMapping("/test")
     private String test() {
+        System.out.println("sout:啊发大水发大水发链接？");
+        log.info("log:啊撒旦解放拉萨的？");
         return "test";
     }
 }

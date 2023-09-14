c0e91ccea020dc16a8e3416d6acc883b2c65a303
CIA0CIAO <mysqlredis@gmail.com>
2023-09-14 22:41:02 +0800

    add：接入Mysql，使用SpringData JPA

--- austin-web/src/main/java/com/ciaociao/austin/web/TestController.java
+++ austin-web/src/main/java/com/ciaociao/austin/web/TestController.java
@@ -1,8 +1,15 @@
 package com.ciaociao.austin.web;
 
+import com.alibaba.fastjson.JSON;
+import com.ciaociao.austin.support.dao.MessageTemplateDao;
+import com.ciaociao.austin.support.domain.MessageTemplate;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author ciaociao
@@ -10,11 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 public class TestController {
+    @Resource
+    private MessageTemplateDao messageTemplateDao;
 
     @RequestMapping("/test")
     private String test() {
         System.out.println("sout:啊发大水发大水发链接？");
         log.info("log:啊撒旦解放拉萨的？");
         return "test";
+    }
+
+    @RequestMapping("/database")
+    private String testDataBase() {
+        List<MessageTemplate> list = messageTemplateDao.findAllByIsDeletedEquals(0, PageRequest.of(0, 10));
+        return JSON.toJSONString(list);
     }
 }
